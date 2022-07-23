c45d21cbb73354496d28e2374a28b2c5e4e4b76a
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-07-23 15:46:53 +0500

    showed content on base of active link and add active class. closes #5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 import Footer from "./components/Footer/Footer";
 import Header from "./components/header/Header";
 
 function App() {
+  const [showContent, setShowContent] = useState("");
+  const showContentHandler = (value) => {
+    setShowContent(value);
+  };
+
+  // console.log(showContent);
+
+  let whichComponentToShow = "";
+
+  switch (showContent) {
+    case "account":
+      whichComponentToShow = <div>create account</div>;
+      break;
+    case "login":
+      whichComponentToShow = <div>login</div>;
+      break;
+    default:
+      whichComponentToShow = <div>main content</div>;
+  }
+
   return (
     <>
-      <Header />
-      <div className="App">Contact App</div>
+      <Header showContent={showContentHandler} />
+      {whichComponentToShow}
       <Footer />
     </>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,78 @@
 import styles from "./Header.module.css";
 
 import logo from "./../../assests/img/logo.png";
+import { useState } from "react";
 
 const Header = (props) => {
+  const [activeLink, setActiveLink] = useState("");
+
+  const activeLinkHandler = (e) => {
+    e.preventDefault();
+
+    setActiveLink(e.target.id);
+
+    props.showContent(e.target.id);
+  };
+
+  // console.log(activeLink);
+
+  let mainActiveClass = "";
+  let loginActiveClass = "";
+  let accountActiveClass = "";
+
+  switch (activeLink) {
+    case "account":
+      accountActiveClass = styles.active;
+      break;
+    case "login":
+      loginActiveClass = styles.active;
+      break;
+    default:
+      mainActiveClass = styles.active;
+  }
+
   return (
     <nav className={styles["nav-bar"]}>
       <div className={styles.logo}>
         <img src={logo} alt="logo" />
         <h1>
-          <a href="/">Contact</a>
+          <a
+            href="/"
+            id="main"
+            onClick={activeLinkHandler}
+            className={mainActiveClass}
+          >
+            Contact
+          </a>
         </h1>
       </div>
+
       <div className={styles.link}>
         <ul>
           <li>
-            <a href="/">Create Account</a>
+            <a
+              href="/"
+              onClick={activeLinkHandler}
+              id="account"
+              className={accountActiveClass}
+            >
+              Create Account
+            </a>
           </li>
+
           <li>
-            <a href="/">Login</a>
+            <a
+              href="/"
+              onClick={activeLinkHandler}
+              id="login"
+              className={loginActiveClass}
+            >
+              Login
+            </a>
           </li>
-          <li>
-            <a href="/">user name</a>
-          </li>
+
+          <li>user name</li>
+
           <li>
             <a href="/">Logout</a>
           </li>

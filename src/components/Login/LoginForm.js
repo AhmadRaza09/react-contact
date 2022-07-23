edb971f006fdab1a028bd749693e31e370ccb79c
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-07-23 22:57:58 +0500

    added register component and remove login bugs. closes #10

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -9,29 +9,13 @@ const LoginForm = (props) => {
     <div className={classes}>
       <div className={styles.card}>
         <form className={styles.form}>
-          <p className={styles["form-title"]}>Create Account</p>
-
-          <fieldset className={styles["form-input"]}>
-            <legend className={styles["input-label"]}>Full Name:</legend>
-            <input />
-            <div className={styles["input-icon"]}>
-              <Person className={styles["input-icon-color"]} />
-            </div>
-          </fieldset>
+          <p className={styles["form-title"]}>Login to Your Account</p>
 
           <fieldset className={styles["form-input"]}>
             <legend className={styles["input-label"]}>Email Address:</legend>
             <input />
             <div className={styles["input-icon"]}>
               <Email className={styles["input-icon-color"]} />
-            </div>
-          </fieldset>
-
-          <fieldset className={styles["form-input"]}>
-            <legend className={styles["input-label"]}>Phone Number:</legend>
-            <input />
-            <div className={styles["input-icon"]}>
-              <Phone className={styles["input-icon-color"]} />
             </div>
           </fieldset>
 
@@ -43,7 +27,7 @@ const LoginForm = (props) => {
             </div>
           </fieldset>
 
-          <button className={styles.button}>Create Account</button>
+          <button className={styles.button}>Login</button>
         </form>
       </div>
     </div>
--- src/components/Register/RegisterUser.js
+++ src/components/Register/RegisterUser.js
@@ -1,12 +1,52 @@
+import { Email, Person, Phone, Security } from "@material-ui/icons";
+
 import styles from "./RegisterUser.module.css";
 
 const RegisterUser = (props) => {
   const classes = `${props.className}`;
 
   return (
-    <p className={classes}>
-      Register yourself on contact application created by Ahmad Raza
-    </p>
+    <div className={classes}>
+      <div className={styles.card}>
+        <form className={styles.form}>
+          <p className={styles["form-title"]}>Create Account</p>
+
+          <fieldset className={styles["form-input"]}>
+            <legend className={styles["input-label"]}>Full Name:</legend>
+            <input />
+            <div className={styles["input-icon"]}>
+              <Person className={styles["input-icon-color"]} />
+            </div>
+          </fieldset>
+
+          <fieldset className={styles["form-input"]}>
+            <legend className={styles["input-label"]}>Email Address:</legend>
+            <input />
+            <div className={styles["input-icon"]}>
+              <Email className={styles["input-icon-color"]} />
+            </div>
+          </fieldset>
+
+          <fieldset className={styles["form-input"]}>
+            <legend className={styles["input-label"]}>Phone Number:</legend>
+            <input />
+            <div className={styles["input-icon"]}>
+              <Phone className={styles["input-icon-color"]} />
+            </div>
+          </fieldset>
+
+          <fieldset className={styles["form-input"]}>
+            <legend className={styles["input-label"]}>Password:</legend>
+            <input />
+            <div className={styles["input-icon"]}>
+              <Security className={styles["input-icon-color"]} />
+            </div>
+          </fieldset>
+
+          <button className={styles.button}>Create Account</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
